822c1541cf32ac49731234521d95157a680790fc
Munsy <tmunson@wsu.edu>
2020-10-15 02:06:33 -0700

    fix index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { MDService } from './src/app/markdown/markdown.service';
-export { MarkdownModule } from './src/app/markdown/markdown.module';
+export { MarkdownModule } from './src/app/markdown.module';
 export { MarkdownComponent } from './src/app/markdown/markdown.component';
